502a9d03b8275389b1938b17c483c87b35d9737c
Debopriyo <Debopriyo@LAPTOP-3DNFLSTK>
2021-05-14 22:01:32 +0530

    commiting to github

---
+++ firstgithubproject/src/com/local/git/file/FirstName.java
@@ -0,0 +1,11 @@
+package com.local.git.file;
+
+public final class FirstName {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Github");
+
+	}
+
+}
